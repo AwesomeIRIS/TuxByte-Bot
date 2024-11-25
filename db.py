a72dc38753b52c7e9b53d052b822d845e613a7d2
Iris <awesomeiris@proton.me>
2024-11-25 10:32:56 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,18 +1,16 @@
 import mysql.connector
 import os
 
-# Function to create a new DB connection and return it along with a cursor
 def get_db_cursor():
     db = mysql.connector.connect(
         host="localhost",
-        user="",  # Replace with your MySQL username
-        password="",  # Replace with your MySQL password
-        database=""  # Replace with your database name
+        user="",
+        password="",
+        database=""
     )
     cursor = db.cursor()
     return db, cursor
 
-# Function to close the DB connection
 def close_db_connection(db, cursor):
     cursor.close()
     db.close()
